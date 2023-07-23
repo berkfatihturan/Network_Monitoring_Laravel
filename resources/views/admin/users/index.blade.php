ef49bc44ac5ec6331c4160f96f921b447c7eef5f
berkfatihturan <bft_@outlook.com>
2023-07-24 00:58:41 +0300

    update profile

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -109,7 +109,8 @@
                                         d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z"/>
                                 </svg>No Authority
                             @endif</td>
-                        <td class="item_delete"><a class="btn btn-danger" href="{{route('admin_users_destroy',['id' => $item->id]) }}">
+                        <td class="item_delete"><a class="btn btn-danger" href="{{route('admin_users_destroy',['id' => $item->id]) }}" @if( \Illuminate\Support\Facades\Auth::id() == $item->id) style="pointer-events: none;"
+                                @endif>
                                 <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512" fill="white">
                                     <path
                                         d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
