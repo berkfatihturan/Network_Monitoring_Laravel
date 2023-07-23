acd75f9fa8e9e2f84727d87f93874dac9521822c
berkfatihturan <bft_@outlook.com>
2023-07-23 19:05:14 +0300

    update profile

--- app/Http/Controllers/AdminPanel/AdminHomeController.php
+++ app/Http/Controllers/AdminPanel/AdminHomeController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\AdminPanel;
 use App\Http\Controllers\Controller;
 use App\Models\Servers;
 use App\Models\Settings;
+use App\Models\User;
+use App\Models\user_login_permission;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Mail;
@@ -27,6 +29,37 @@ class AdminHomeController extends Controller
     {
 
 
+    }
+
+    public function loginadmin(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'=>['required','email'],
+            'password'=>['required'],
+        ]);
+
+        if(User::first()==null){
+            $adminUser = new User();
+            $adminUser->name = "admin";
+            $adminUser->email = "admin@admin.com";
+            $adminUser->password = '$2y$10$vHKfy.J5AesxLxiFuL680OBjedy0MF5V11aYAO1Tzs2SOWuAM9sT.';
+            $adminUser->save();
+
+            $user=User::first();
+            $userPermission = new user_login_permission();
+            $userPermission->user_id = $user->id;
+            $userPermission->is_allowed = true;
+            $userPermission->save();
+        }
+
+        if (Auth::attempt($credentials)){
+            $request->session()->regenerate();
+            return redirect()->intended('/admin');
+        }
+
+        return back()->withErrors([
+            'error'=>'The provided credentials do not match our record.',
+        ]);
     }
 
 
---
+++ app/Http/Controllers/AdminPanel/AdminUsersController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\AdminPanel;
+
+use App\Http\Controllers\Controller;
+use App\Models\Settings;
+use App\Models\User;
+use App\Models\user_login_permission;
+use Illuminate\Http\Request;
+
+class AdminUsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $settingsData = Settings::first();
+        $userData = User::all();
+
+        return view('admin.users.index',[
+            'settingsData' => $settingsData,
+            'userData' =>$userData
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request)
+    {
+        $userData = User::all();
+        foreach ($userData as $user){
+            $itemId = $request->input('is_allowed-' . $user->id);
+
+            $data = $user->user_login_permission()->first();
+            if($data==null){
+                $data = new user_login_permission();
+            }
+
+            $data->user_id = $user->id;
+            if ($itemId === 'on') {
+                $data->is_allowed = true;
+            }else{
+                $data->is_allowed = false;
+            }
+            $data->save();
+        }
+
+        return redirect('admin/users');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
