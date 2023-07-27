8e1e7d6ff2a12f57bc5f2a808036804310c186ea
berkfatihturan <bft_@outlook.com>
2023-07-27 16:31:17 +0300

    Merge remote-tracking branch 'origin/master'

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,7 +18,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('app:check-database')->everyMinute()->withoutOverlapping();
+        $schedule->command('app:check-database')->everyMinute()->runInBackground();
         $schedule->command('app:check-ports')->everyMinute()->runInBackground();
 
         // $schedule->command('inspire')->hourly();
