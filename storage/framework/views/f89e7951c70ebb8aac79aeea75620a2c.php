43ef2211f33d205332312ace0e123d79d8c2a7a0
berkfatihturan <bft_@outlook.com>
2023-07-28 13:20:08 +0300

    Merge remote-tracking branch 'origin/master'

--- storage/framework/views/f89e7951c70ebb8aac79aeea75620a2c.php
+++ storage/framework/views/f89e7951c70ebb8aac79aeea75620a2c.php
@@ -2,16 +2,20 @@
 <p>UPDATE</p>
 <hr/>
 
-<form action="<?php echo e(route('admin_server_update')); ?>" method="POST" enctype="multipart/form-data" >
+<form action="<?php echo e(route('admin_server_update')); ?>" method="POST" enctype="multipart/form-data">
     <?php echo csrf_field(); ?>
-    <input type="hidden" name="id">
+    <input type="hidden" name="id" required>
 
     <label for="fname">Server Name:</label><br>
-    <input type="text" name="server_name"><br>
+    <input type="text" name="server_name" required><br>
 
 
     <label for="lname">Ip:</label><br>
-    <input type="text" name="ip"><br>
+    <input type="text" name="ip" required><br>
+
+
+    <p style="margin-bottom: 5px"><label for="detail">Details:</label></p>
+    <textarea name="detail" rows="4" style="width: 100%"></textarea>
 
 
     <input type="submit" value="Submit"
