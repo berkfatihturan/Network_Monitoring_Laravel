daf30c747faf96d0aebfb5a644d5dd4aec67cde3
berkfatihturan <bft_@outlook.com>
2023-07-28 13:19:05 +0300

    Merge remote-tracking branch 'origin/master'

---
+++ storage/framework/views/a58b72eb22bb767e9f0b4e946f632376.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH D:\BFT\Project\Laravel\project_x\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
