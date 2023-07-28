93bb62b0d3e638977bb2f5175f5f40d08c246e43
berkfatihturan <bft_@outlook.com>
2023-07-28 13:18:45 +0300

    Merge remote-tracking branch 'origin/master'

--- storage/framework/views/3e47a92c1cbb2d5da1e55c60838671b5.php
+++ storage/framework/views/3e47a92c1cbb2d5da1e55c60838671b5.php
@@ -45,12 +45,12 @@
             <?php echo csrf_field(); ?>
             <div class="col-lg-3 col-sm-12">
                 <label for="fname">Server Name:</label><br>
-                <input type="text" name="server_name"><br>
+                <input type="text" name="server_name" required><br>
 
             </div>
             <div class="col-lg-3 col-sm-12">
                 <label for="lname">Ip:</label><br>
-                <input type="text" name="ip"><br><br>
+                <input type="text" name="ip" required><br><br>
             </div>
 
             <div>
