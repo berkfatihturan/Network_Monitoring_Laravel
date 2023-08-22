6742b803c3f037b599b2d9fb75b8f9fb50cc5338
berkfatihturan <bft_@outlook.com>
2023-08-22 11:03:56 +0300

    update header

---
+++ storage/framework/views/c9ff5fa29cd19a7163f28bb0c70bd812.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH X:\BFT\Project\Laravel\project_x\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
